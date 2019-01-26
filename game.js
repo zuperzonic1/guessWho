da9f038ff129e54697e72b1b5a16e0169e693516
Stuart Sackler <stuart.sackler@gmail.com>
2019-01-26 15:57:14 -0500

    adding the project files to the repo

---
+++ game.js
@@ -0,0 +1,36 @@
+let attendees = [
+    {firstName:"Nelson", lastName:"Flanders", birthYear:1977, occupation:"Accountant", gender:"M", homePlanet:"Earth"},
+    {firstName:"Frank", lastName:"Smith", birthYear:1940, occupation:"Retiree", gender:"M", homePlanet:"Earth"},
+    {firstName:"Stella", lastName:"Simpson", birthYear:2000, occupation:"Engineer", gender:"F", homePlanet:"Earth"},
+    {firstName:"Sam", lastName:"Bertram", birthYear:1910, occupation:"Retiree", gender:"F", homePlanet:"Earth"},
+    {firstName:"Tanis", lastName:"Lane", birthYear:2004, occupation:"Student", gender:"M", homePlanet:"Earth"},
+    {firstName:"Adele", lastName:"Smith", birthYear:1967, occupation:"Teacher", gender:"F", homePlanet:"Earth"},
+    {firstName:"Fran", lastName:"Arkel", birthYear:1998, occupation:"Transit Operator", gender:"F", homePlanet:"Earth"},
+    {firstName:"Wilson", lastName:"Depp", birthYear:2016, occupation:"Golfer", gender:"M", homePlanet:"Earth"},
+    {firstName:"Naomi", lastName:"Lange", birthYear:1984, occupation:"Wrestler", gender:"F", homePlanet:"Earth"},
+    {firstName:"Sven", lastName:"Ford", birthYear:1980, occupation:"Tour Guide", gender:"M", homePlanet:"Earth"},
+    {firstName:"Laila", lastName:"Martin", birthYear:1979, occupation:"Lawyer", gender:"F", homePlanet:"Earth"},
+    {firstName:"Angus", lastName:"Tampo", birthYear:2010, occupation:"Student", gender:"M", homePlanet:"Earth"},
+    {firstName:"Franny", lastName:"Loo", birthYear:2017, occupation:"Infant", gender:"F", homePlanet:"Earth"},
+    {firstName:"Louise", lastName:"Monroe", birthYear:1986, occupation:"Transit Operator", gender:"F", homePlanet:"Earth"},
+    {firstName:"Olna", lastName:"Hampkin", birthYear:1986, occupation:"Student", gender:"F", homePlanet:"Earth"}
+  ]
+  
+  
+  //find the attendees born in the 1970's using filter
+  
+  
+  //from the attendees born in the 70's, using filter find me the accountant and console.log his first name
+  
+  
+  //using map return a list of peoples first names and birthYear in the format of Name: birthYear
+  
+  
+  //using some find out if there is someone with the occupation of infant
+  
+  
+  //find the array index of the a transit operator named Louise
+  
+  
+  //using sort return a list of the student attendees by birth year in ascending order
+  
