8af2af56074860a277cc75ca190612be35ef6467
Stuart Sackler <stuart.sackler@gmail.com>
2019-01-27 00:49:53 -0500

    updating people list

--- game.js
+++ game.js
@@ -1,26 +1,31 @@
 let attendees = [
-    {firstName:"Nelson", lastName:"Flanders", birthYear:1977, occupation:"Accountant", gender:"M", homePlanet:"Earth"},
-    {firstName:"Frank", lastName:"Smith", birthYear:1940, occupation:"Retiree", gender:"M", homePlanet:"Earth"},
-    {firstName:"Stella", lastName:"Simpson", birthYear:2000, occupation:"Engineer", gender:"F", homePlanet:"Earth"},
-    {firstName:"Sam", lastName:"Bertram", birthYear:1910, occupation:"Retiree", gender:"F", homePlanet:"Earth"},
-    {firstName:"Tanis", lastName:"Lane", birthYear:2004, occupation:"Student", gender:"M", homePlanet:"Earth"},
-    {firstName:"Adele", lastName:"Smith", birthYear:1967, occupation:"Teacher", gender:"F", homePlanet:"Earth"},
-    {firstName:"Fran", lastName:"Arkel", birthYear:1998, occupation:"Transit Operator", gender:"F", homePlanet:"Earth"},
-    {firstName:"Wilson", lastName:"Depp", birthYear:2016, occupation:"Golfer", gender:"M", homePlanet:"Earth"},
-    {firstName:"Naomi", lastName:"Lange", birthYear:1984, occupation:"Wrestler", gender:"F", homePlanet:"Earth"},
-    {firstName:"Sven", lastName:"Ford", birthYear:1980, occupation:"Tour Guide", gender:"M", homePlanet:"Earth"},
-    {firstName:"Laila", lastName:"Martin", birthYear:1979, occupation:"Lawyer", gender:"F", homePlanet:"Earth"},
-    {firstName:"Angus", lastName:"Tampo", birthYear:2010, occupation:"Student", gender:"M", homePlanet:"Earth"},
-    {firstName:"Franny", lastName:"Loo", birthYear:2017, occupation:"Infant", gender:"F", homePlanet:"Earth"},
-    {firstName:"Louise", lastName:"Monroe", birthYear:1986, occupation:"Transit Operator", gender:"F", homePlanet:"Earth"},
-    {firstName:"Olna", lastName:"Hampkin", birthYear:1986, occupation:"Student", gender:"F", homePlanet:"Earth"}
+    {firstName:"Robin", lastName:"Banks", birthYear:1977, occupation:"Accountant", gender:"M", homePlanet:"Earth"},
+    {firstName:"Felix", lastName:"Cited", birthYear:1940, occupation:"Retiree", gender:"M", homePlanet:"Earth"},
+    {firstName:"Snorkfloz", lastName:"Thzbert", birthYear:1800, occupation:"Tentacle Mangler", gender:"X", homePlanet:"Zerkblatt"},
+    {firstName:"Maxie", lastName:"Mumm", birthYear:2000, occupation:"Engineer", gender:"F", homePlanet:"Earth"},
+    {firstName:"Gerry", lastName:"Atrick", birthYear:1910, occupation:"Retiree", gender:"F", homePlanet:"Earth"},
+    {firstName:"Yul", lastName:"Beefine", birthYear:2004, occupation:"Student", gender:"M", homePlanet:"Earth"},
+    {firstName:"Eileen", lastName:"Dover", birthYear:1967, occupation:"Teacher", gender:"F", homePlanet:"Earth"},
+    {firstName:"Rhoda", lastName:"Camel", birthYear:1998, occupation:"Transit Operator", gender:"F", homePlanet:"Earth"},
+    {firstName:"Wilsin", lastName:"Yorname", birthYear:2016, occupation:"Golfer", gender:"M", homePlanet:"Earth"},
+    {firstName:"Shlerznak", lastName:"Flabadabs", birthYear:1700, occupation:"Ion Thruster Repair", gender:"F", homePlanet:"Zerkblatt"},
+    {firstName:"Sandy", lastName:"Beech", birthYear:1984, occupation:"Wrestler", gender:"F", homePlanet:"Earth"},
+    {firstName:"Luke", lastName:"Overdehre", birthYear:1980, occupation:"Tour Guide", gender:"M", homePlanet:"Earth"},
+    {firstName:"Ima", lastName:"Suyu", birthYear:1979, occupation:"Lawyer", gender:"F", homePlanet:"Earth"},
+    {firstName:"Knottaner", lastName:"Thlink", birthYear:1991, occupation:"Chef", gender:"X", homePlanet:"Cranth"},
+    {firstName:"Al", lastName:"Knighter", birthYear:2010, occupation:"Student", gender:"M", homePlanet:"Earth"},
+    {firstName:"Ivana", lastName:"Tinkle", birthYear:2017, occupation:"Infant", gender:"F", homePlanet:"Earth"},
+    {firstName:"Francis", lastName:"Fulla-Franschpeeple", birthYear:2017, occupation:"Chief Biscuit Dunker", gender:"M", homePlanet:"Earth"},
+    {firstName:"Lori", lastName:"Driver", birthYear:1986, occupation:"Transit Operator", gender:"F", homePlanet:"Earth"},
+    {firstName:"Doris", lastName:"Shut", birthYear:1986, occupation:"Student", gender:"F", homePlanet:"Earth"},
+    {firstName:"Levi", lastName:"Tate", birthYear:1966, occupation:"Magician", gender:"M", homePlanet:"Earth"}
   ]
   
   
   //find the attendees born in the 1970's using filter
   
   
-  //from the attendees born in the 70's, using filter find me the accountant and console.log his first name
+  //from the attendees born in the 70's, using filter find me the accountant and console.log their first name
   
   
   //using map return a list of peoples first names and birthYear in the format of Name: birthYear
@@ -29,8 +34,13 @@ let attendees = [
   //using some find out if there is someone with the occupation of infant
   
   
-  //find the array index of the a transit operator named Louise
+  //find the array index of the a Chief Biscuit Dunker named Francis
   
   
   //using sort return a list of the student attendees by birth year in ascending order
+
+
+  //Return a list of beings that are not from Earth who are Gender X
+
+  //
   
